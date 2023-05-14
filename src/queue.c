5bf6f3a57262ed6cd807e815fb109e8d8afa6aa7
hungvo2003vn <votanhung2003@gmail.com>
2023-05-14 21:37:03 +0700

    align comment

--- src/queue.c
+++ src/queue.c
@@ -17,8 +17,8 @@ void enqueue(struct queue_t * q, struct pcb_t * proc) {
 
 struct pcb_t * dequeue(struct queue_t * q) {
 	/* TODO: return a pcb whose prioprity is the highest
-		* in the queue [q] and remember to remove it from q
-		* */
+	* in the queue [q] and remember to remove it from q
+	* */
 
     if (q->size == 0) return NULL;
 
